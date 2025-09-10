394fc28bbd384abe3706f3f315c9d582b1bf13e9
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-09-10 20:03:23 +0530

    feat: added prerendering after build"

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    target: 'es2017', // or 'es2019'
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
